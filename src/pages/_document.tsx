5831c7aa3342ebd6e789cbb5f7ea4c682ad18fa9
Stephen Cooper <ste.cooper@live.co.uk>
2024-04-05 22:27:15 +0100

    lol

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="og:title" content="Stephen Cooper - CV" />
+        <meta name="og:description" content="Welcome to my CV. Hire me. Go on." />
+        <meta name="og:image" content="thumb.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
