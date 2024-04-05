c4c0dc9bb9396ed84632928794215280b9d09fe8
Stephen Cooper <ste.cooper@live.co.uk>
2024-04-05 22:08:46 +0100

    More WIP

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,129 +1,248 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import ListGroup from "react-bootstrap/ListGroup";
+
+interface IContactDetails {
+  emailAddress: string,
+  mobileNumber: string
+}
+
+interface ICertification {
+  name: string,
+  provider: string,
+  date: string,
+  outcome: string,
+  description: string
+}
+
+interface ISocial {
+  name: string,
+  url: string
+}
+
+interface IProject {
+  name: string,
+  url: string,
+  description: string
+}
+
+interface IOSContribution {
+  url: string,
+  description: string,
+  name: string
+}
+
+interface IJob {
+  name: string,
+  company: string,
+  date: string,
+  description: string
+}
+
+interface ICV {
+  name: string,
+  jobTitle: string,
+  contactDetails: IContactDetails,
+  introduction: string[],
+  socials: ISocial[],
+  knownTechnologies: string[],
+  projects: IProject[],
+  openSourceContributions: IOSContribution[],
+  certifications: ICertification[],
+  jobs: IJob[]
+}
 
 const inter = Inter({ subsets: ["latin"] });
 
-interface ITestProps {
-  one: string
+interface ICVComponentProps {
+  cv: ICV
 }
 
-export function Test({ one }: ITestProps) {
+export function CVComponent({ cv }: ICVComponentProps) {
   return (
-    <h1>Hello {one}</h1>
+    <Container>
+      <Row>
+        <Col xs={8} className="gray text-white">
+          <h1 className="fs-1 p-5">{cv.name}</h1>
+        </Col>
+        <Col xs={4} className="blue text-white col-4 d-flex justify-content-center align-items-center">
+          <p className="fs-4">{cv.jobTitle}</p>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={8} className="blue text-white">
+        </Col>
+        <Col xs={4} className="gray text-white d-flex justify-content-center align-items-center">
+          <h2 className="fs-4 p-3">Contact</h2>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={8} className="bg-white">
+          {cv.introduction.map((intro, i) => <p key={i} className="p-4">{intro}</p>)}
+        </Col>
+        <Col xs={4} className="gray2">
+            {cv.socials.map(({ name, url }, i) => 
+              <div key={i}className="pt-3 text-center">
+                <a className="w-100 text-primary" href={url}><u>{name}</u></a>
+              </div>
+            )}
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={8} className="gray text-white d-flex justify-content-center align-items-center">
+          <h2 className="fs-4 p-3">Employeement</h2>
+        </Col>
+        <Col xs={4} className="blue text-white d-flex justify-content-center align-items-center">
+          <h2 className="fs-4 p-3">Technologies</h2>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={8} className="bg-white">
+          <div className="p-4">
+            {cv.jobs.map((job) => 
+              <div>
+                <h3 className="fs-5">{job.name} - <small>{job.company}</small></h3>
+                <p className="pt-2 fs-6 text-primary">{job.date}</p>
+                <p className="pt-2 fs-6 pb-3">{job.description}</p>
+              </div>
+            )}
+          </div>
+        </Col>
+        <Col xs={4} className="gray2 p-0">
+          <div className="d-flex flex-wrap">
+            {cv.knownTechnologies.map((t, i) => 
+              <p className="ps-3 pe-3 pt-3" key={i}>{t}</p>
+            )}
+          </div>
+          
+          <div className="gray text-white mt-3">
+            <h2 className="fs-4 p-3">
+              Education
+            </h2>
+          </div>
+
+          <div className="p-3">
+            {cv.certifications.map((cert) => 
+              <div>
+                <h3 className="fs-5">{cert.name} - <small>{cert.provider}</small></h3>
+                <p className="pt-3 fs-6 text-primary">{cert.date}</p>
+                <p className="pt-1 fs-6">{cert.outcome}</p>
+                <p className="pt-2 fs-6">{cert.description}</p>
+              </div>
+            )}
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="blue text-white">
+          <h2 className="fs-4 p-3"> Personal Projects </h2>
+        </Col>
+      </Row>
+      {cv.projects.map(({ name, url, description }, i) => (
+        <Row key={i}>
+          <h2>{name}</h2>
+          <Button href={url}>{url}</Button>
+          <p>{description}</p>
+        </Row>))
+      }
+      <Row>
+        <h2>Contributions to Open-Source projects</h2>
+        <p>I’ve performed multiple contributions to open-source projects.</p>
+        <ul>
+          {cv.openSourceContributions.map(({ name, url, description}, i) =>
+              <li key={i}>
+                <Row>
+                  <Col><Button href={url}>{name}</Button></Col>
+                  <Col>{description}</Col>
+                </Row>
+            </li>
+          )}
+        </ul>
+      </Row>
+    </Container>
   );
 }
 
 export default function Home() {
+  const cv: ICV = {
+    name: 'Stephen Cooper',
+    jobTitle: 'Software Engineer',
+    contactDetails: {
+      emailAddress: 'ste.cooper@live.co.uk',
+      mobileNumber: ''
+    },
+    socials: [{
+      name: 'Github Repositories',
+      url: 'https://github.com/ste2425'
+    }, {
+      name: 'Twitter',
+      url: 'https://twitter.com/_s_cooper'
+    }, {
+      name: 'ste.cooper@live.co.uk',
+      url: 'mailto:ste.cooper@live.co.uk'
+    }, {
+      name: 'https://ste2425.github.io/',
+      url: 'https://ste2425.github.io/'
+    }],
+    introduction: ['I\’m a software engineer with experience in the HR, Payroll and Financial Regulation sectors. I recently led a team as a Senior developer working on greenfield projects migrating legacy systems to new platforms. Currently I work in a team of peers building fantastic features which provide value to our customers.', 'I have a number of personal projects which I use to learn technologies i don\'t get the opportunity to at work. Most recently being C++ in an embedded ESP32 environment where I\'m implementing Bluetooth controller support in my Sony PSP.'],
+    knownTechnologies: ['NodeJS', 'NestJS', 'Angular', 'React', '.NET Core', 'MVC', 'NextJS', 'Bulbasour'],
+    projects: [{
+      name: 'Dotnet Runner',
+      url: 'https://github.com/ste2425/DotnetRunner',
+      description: 'An ElectronJS based application to launch multiple .NET core based applications. Whilst working at Cascade we had a number of .NET core based applications and launching each individual was tedious. I wanted to learn Electron so decided to build a little app which could launch multiple for me from a easy to access context menu in the hosting operating systems tray.'
+    }, {
+      name: 'PSP Consolizer',
+      url: 'https://github.com/ste2425/PSP-Consolizer',
+      description: 'A project to allow the use of Bluetooth controllers with Sony’s handheld console the PSP. This involved an Arduino NANO 33 IOT, a custom designed PCB and an ElectronJS based installer application. This was a very involved project which required learning multiple new technologies in hardware and software.'
+    }],
+    openSourceContributions: [{
+      name: 'VitaDock Plus',
+      url: 'https://github.com/SilentNightx/VitaDockPlus',
+      description: 'Added the ability to use Aux audio and control via GPIO buttons.'
+    }, {
+      name: 'ts-json-object',
+      url: 'https://github.com/moshegottlieb/ts-json-object',
+      description: 'Created PR to change how arrays are validated'
+    }],
+    certifications: [{
+      name: 'Music Technology & Software Development',
+      provider: 'University of huddersfield',
+      date: '2008 - 2012',
+      outcome: 'Completed in 2012 with a 2:2',
+      description: 'Degree focused on music technology including live and studio-based recordings. Final year had a software development module which sparked my interest in programming and lead me to seek a career in this field.'
+    }],
+    jobs: [{
+      name: 'Web Developer',
+      company: 'RegTransfers',
+      date: '2023 - PRESENT',
+      description: 'Part of a four-man team of highly experienced developers. My responsibilities are the maintenance of the various systems and infrastructure and to provide new value rich features for the business. We maintain the Kubernetes infrastructure and manage it with Flux. The solution is a highly distributed micro service architecture where services extensively leverage event based communications. My most recently built feature is a search assistant tool built using Web Components (the first in the system, hopefully not the last!).'
+    }, {
+      name: 'Senior Developer',
+      company: 'Trailight',
+      date: '2021-2023',
+      description: 'Senior developer in a team of three other developers taking on team lead responsibilities. Providing mentoring for other team members who have all increased to mid-level developers from juniors.Working on a greenfield project to combine functionality from two existing monolith applications.Primary responsibilities are owning the architectural implementation and code quality of a React based application utilizing MFE\’s as well as multiple RESTand GraphQL based services built with NestJS.'
+    }, {
+      name: 'Web Developer',
+      company: 'Cascade HR (IRIS)',
+      date: '2013 - 2021',
+      description: 'Member of the core HR team. Worked primarily on the greenfield rewrite project on the HR application. This was an Angular based UI with .NET core based backend REST services.This was a small team of three developers who were leading the way with technologies that were entirely new to the business.Took ownership of the UI architecture.I performed many tech-talks to the other teams as part of knowledge sharing during this project'
+    }, {
+      name: 'Junior Web Developer',
+      company: 'Cascade HR',
+      date: '2012 - 2013',
+      description: 'First role out of university. This time was spent doing extensive self-study. Tasks performed involved basic UI changes and bug fixes in a system used byover a thousand customers.'
+    }]
+  };
+
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between ${inter.className}`}
     >
-      <Test one="BOY"/>
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <CVComponent cv={cv}/>
     </main>
   );
 }
