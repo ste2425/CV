23ed25d02592bfe6bfaa21f49436ed80ac300512
Stephen Cooper <ste.cooper@live.co.uk>
2024-04-06 21:14:49 +0100

    fix accidental commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  //output: 'export',
-  //basePath: '/CV'
+  output: 'export',
+  basePath: '/CV'
 };
 
 export default nextConfig;
