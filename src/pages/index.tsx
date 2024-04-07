5b364cf3da46c2adee9dd8afdb486808819dd8cd
Stephen Cooper <ste.cooper@live.co.uk>
2024-04-07 21:58:34 +0100

    Finalized content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -94,7 +94,7 @@ export function CVComponent({ cv }: ICVComponentProps) {
     <Container>
       <Row>
         <Col xs={12} md={8} className="gray text-white flex-column d-flex justify-content-center align-items-center">
-          <h1 className="fs-1 p-5 pb-3 text-center">{cv.name}</h1>
+          <h1 className="fs-1 p-5 pb-1 text-center">{cv.name}</h1>
           <a className="text-primary generate-message" href="StephenCooper-CV.pdf" download="StephenCooper-CV.pdf"><u>Download as PDF</u></a>
         </Col>
         <Col xs={12} md={4} className="blue text-white col-4 d-flex justify-content-center align-items-center">
@@ -214,38 +214,53 @@ export default function Home() {
       emailAddress: 'ste.cooper@live.co.uk',
       mobileNumber: ''
     },
-    socials: [{
+    socials: [ {
+      name: 'https://ste2425.github.io/',
+      url: 'https://ste2425.github.io/'
+    }, {
+      name: 'ste.cooper@live.co.uk',
+      url: 'mailto:ste.cooper@live.co.uk'
+    }, {
       name: 'Github Repositories',
       url: 'https://github.com/ste2425'
     }, {
       name: 'Twitter',
       url: 'https://twitter.com/_s_cooper'
-    }, {
-      name: 'ste.cooper@live.co.uk',
-      url: 'mailto:ste.cooper@live.co.uk'
-    }, {
-      name: 'https://ste2425.github.io/',
-      url: 'https://ste2425.github.io/'
     }],
-    introduction: ['I\’m a software engineer with experience in the HR, Payroll and Financial Regulation sectors. I recently led a team as a Senior developer working on greenfield projects migrating legacy systems to new platforms. Currently I work in a team of peers building fantastic features which provide value to our customers.', 'I have a number of personal projects which I use to learn technologies i don\'t get the opportunity to at work. Most recently being C++ in an embedded ESP32 environment where I\'m implementing Bluetooth controller support in my Sony PSP.'],
-    knownTechnologies: ['NodeJS', 'NestJS', 'Angular', 'React', '.NET Core', 'MVC', 'NextJS', 'Bulbasour'],
+    introduction: [
+      'I\'m a software engineer with experience in the HR, Payroll and Financial Regulation sectors. I recently led a team as a Senior developer working on greenfield projects migrating legacy systems to new platforms. Currently I work in a team of three other very skilled engineers. We collaborate closely designing and building new features as well as tackling bugs and technical debt to provide value to our customers. ',
+     'I have several personal projects which I use to learn technologies I don\'t get the opportunity to use at work. Most recently being C++ in an embedded ESP32 environment where I\'m implementing Bluetooth controller support in my Sony PSP. '
+    ],
+    knownTechnologies: ['NodeJS', 'NestJS', 'Angular', 'React', '.NET Core', 'MVC', 'Banjo', 'NextJS', 'Kazooie'],
     projects: [{
       name: 'Dotnet Runner',
       url: 'https://github.com/ste2425/DotnetRunner',
-      description: 'An ElectronJS based application to launch multiple .NET core based applications. Whilst working at Cascade we had a number of .NET core based applications and launching each individual was tedious. I wanted to learn Electron so decided to build a little app which could launch multiple for me from a easy to access context menu in the hosting operating systems tray.'
+      description: 'An ElectronJS based application to launch multiple .NET core applications. Whilst working at Cascade we had several .NET core applications and launching everyone individually was tedious. I wanted to learn Electron so decided to build a little app which could launch multiple for me from an easy to access context menu in the hosting operating systems tray. In hindsight IDE integrations would have made this somewhat redundant but it was a fun learning experience for technologies i had not used at this point. '
+    }, {
+      name: 'ESP32 OTA update Web Portal',
+      url: 'https://github.com/ste2425/OTAWebapp',
+      description: 'This is a webapp I built as part of my contributions to BluePad32. It utilizes the Web Bluetooth APIs to demonstrate communications between a web browser and a BLE enabled device, in this case an ESP32. Its current implementation supports an OTA (Over the Air Update) process. This allows programming of the ESP32 via Bluetooth.  '
     }, {
       name: 'PSP Consolizer',
       url: 'https://github.com/ste2425/PSP-Consolizer',
-      description: 'A project to allow the use of Bluetooth controllers with Sony’s handheld console the PSP. This involved an Arduino NANO 33 IOT, a custom designed PCB and an ElectronJS based installer application. This was a very involved project which required learning multiple new technologies in hardware and software.'
+      description: 'A project to allow the use of Bluetooth controllers with Sony\'s handheld console the PSP. This is a long running project which i am still working on currently. I learnt many new things during this project, working on embedded systems and writing in C++, hardware development and PCB design. The first iteration was an eternal module that connected to the PSP\'s internal connectors. The second iteration I\'m currently working is ported to an ESP32 and miniaturized to fit inside the PSP (replacing the PSP\'s UMD drive) '
+    }, {
+      name: 'Electron CAN Viewer',
+      url: 'https://github.com/ste2425/Electron-CAN-viewer',
+      description: 'Before building my PSP Consolizer I started a project (I intend to return to) to add extra functionality to my car, features such as power folding mirrors, auto closing/opening window, welcome light etc. It was originally powered by an Arduino but will most likely be powered by an ESP32. This project allowed me to view the CANBus messages sent between the internal modules in my vehicle so I could intercept messages I needed relating to things like engine/road speed and the status of the door locks. I have big ideas for this project, using an ESP32 and writing a react native app to use BLE to control things in my car. I can then add support for Apple CarPlay and control things like ambient lighting via a custom interface in CarPlay. '
     }],
     openSourceContributions: [{
+      name: 'BluePad32',
+      url: 'https://github.com/ricardoquesada/bluepad32',
+      description: 'This is a fantastic library that allows communication with Bluetooth based controllers on ESP32\'s and other microcontrollers. I use it heavily in my PSP Consolizer and worked closely with the maintainer to integrate OTA (Over the Air Updates) and general BLE two-way data communications whilst the library is also communicating with controllers. Up, until this point that was not possible. My implementation is now, currently, the recommended solution and provided as an example by BluePad32 for people using the Arduino IDE.'
+    }, {
       name: 'VitaDock Plus',
       url: 'https://github.com/SilentNightx/VitaDockPlus',
-      description: 'Added the ability to use Aux audio and control via GPIO buttons.'
+      description: 'This is a project that allow a modified PlayStation Vita to project its video to a tv using a Raspberry PI. I added the ability to control the Raspberry PI via GPIO buttons and allow audio to be captured via AUX input instead of Bluetooth. This was a fun project as I had done little Linux scripting outside of simple CI scripts. '
     }, {
       name: 'ts-json-object',
       url: 'https://github.com/moshegottlieb/ts-json-object',
-      description: 'Created PR to change how arrays are validated'
+      description: 'I created a PR to help solve a bug regarding validating arrays. Unfortunately, my implementation did not land but I worked with the maintainer to help come to the final solution. '
     }],
     certifications: [{
       name: 'Music Technology & Software Development',
@@ -258,22 +273,22 @@ export default function Home() {
       name: 'Web Developer',
       company: 'RegTransfers',
       date: '2023 - PRESENT',
-      description: 'Part of a four-man team of highly experienced developers. My responsibilities are the maintenance of the various systems and infrastructure and to provide new value rich features for the business. We maintain the Kubernetes infrastructure and manage it with Flux. The solution is a highly distributed micro service architecture where services extensively leverage event based communications. My most recently built feature is a search assistant tool built using Web Components (the first in the system, hopefully not the last!).'
+      description: 'Part of a four-man team of highly experienced developers. My responsibilities are the maintenance of the various systems and infrastructure and to provide new value rich features for the business. I can work independently throughout all the stack layers as well collaborate with other team members. We maintain the Kubernetes infrastructure and manage it with Flux. The solution is a highly distributed micro service architecture where services extensively leverage event-based communications. My most recently built feature is a search assistant tool built using Web Components (the first in the system, hopefully not the last!). '
     }, {
       name: 'Senior Developer',
       company: 'Trailight',
       date: '2021-2023',
-      description: 'Senior developer in a team of three other developers taking on team lead responsibilities. Providing mentoring for other team members who have all increased to mid-level developers from juniors.Working on a greenfield project to combine functionality from two existing monolith applications.Primary responsibilities are owning the architectural implementation and code quality of a React based application utilizing MFE\’s as well as multiple RESTand GraphQL based services built with NestJS.'
+      description: 'Senior developer in a team of three other developers taking on team lead responsibilities. Providing mentoring for other team members who have all increased to mid-level developers since starting as juniors. I was responsible for taking a lead design role, alongside our technical architect and implementation role to build a greenfield system which integrated functionality of two legacy monolith applications. I owned the implementation and was responsible for maintaining code quality of a React based application utilizing MFE\'s as well as multiple REST and GraphQL based services built with NestJS. '
     }, {
       name: 'Web Developer',
       company: 'Cascade HR (IRIS)',
       date: '2013 - 2021',
-      description: 'Member of the core HR team. Worked primarily on the greenfield rewrite project on the HR application. This was an Angular based UI with .NET core based backend REST services.This was a small team of three developers who were leading the way with technologies that were entirely new to the business.Took ownership of the UI architecture.I performed many tech-talks to the other teams as part of knowledge sharing during this project'
+      description: 'Member of the core HR team. Worked primarily on the greenfield rewrite project on the HR application. This was an Angular based UI with .NET core based backend REST services. This was a small team of three developers who were leading the way with technologies that were entirely new to the business. Took ownership of the UI architecture. I performed many tech-talks to the other teams as part of knowledge sharing during this project '
     }, {
       name: 'Junior Web Developer',
       company: 'Cascade HR',
       date: '2012 - 2013',
-      description: 'First role out of university. This time was spent doing extensive self-study. Tasks performed involved basic UI changes and bug fixes in a system used byover a thousand customers.'
+      description: 'First role out of university. This time was spent doing extensive self-study. Tasks performed involved basic UI changes and bug fixes in a system used by over a thousand customers.  '
     }]
   };
 
